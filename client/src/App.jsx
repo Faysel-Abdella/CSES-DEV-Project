a52a3f57ead2d569ee3ec4e4941a9c58bd90005c
faysel abdella <fayselcode@gmail.com>
2023-11-25 17:25:55 +0300

    Add a utility function to make fetching more easy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import ErrorPage from "./pages/404";
+
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Features from "./components/Features";
@@ -6,18 +10,69 @@ import LandingEvents from "./components/LandingEvents";
 import NavBar from "./components/NavBar";
 import Testimonial from "./components/Testimonial";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    element: (
+      <>
+        <NavBar />
+        <Home />
+        <About />
+        <LandingEvents />
+        <Features />
+        <Testimonial />
+        <Contact />
+      </>
+    ),
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+        action: loginAction,
+      },
+      // {
+      //   path: "dashboard",
+      //   element: <DashboardLayout isDarkThemeEnabled={isDarkThemeEnabled} />,
+      //   loader: dashboardLoader,
+      //   children: [
+      //     {
+      //       index: true,
+      //       element: <AddJob />,
+      //       action: addJobAction,
+      //     },
+      //     { path: "stats", element: <Stats />, loader: statsLoader },
+      //     {
+      //       path: "all-jobs",
+      //       element: <AllJobs />,
+      //       loader: allJobsLoader,
+      //     },
+
+      //     {
+      //       path: "profile",
+      //       element: <Profile />,
+      //       action: profileAction,
+      //     },
+      //     {
+      //       path: "admin",
+      //       element: <Admin />,
+      //       loader: adminLoader,
+      //     },
+      //     {
+      //       path: "edit-job/:id",
+      //       element: <EditJob />,
+      //       loader: editJobLoader,
+      //       action: editPageAction,
+      //     },
+      //     { path: "delete-job/:id", action: deleteJobAction },
+      //   ],
+      // },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <NavBar />
-      <Home />
-      <About />
-      <LandingEvents />
-      <Features />
-      <Testimonial />
-      <Contact />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
