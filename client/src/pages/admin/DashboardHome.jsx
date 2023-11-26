393c0130c1d0e10e45ace6a69ded41f423d6274c
faysel abdella <fayselcode@gmail.com>
2023-11-26 17:34:27 +0300

    Create the home page of the  admin

--- client/src/pages/admin/DashboardHome.jsx
+++ client/src/pages/admin/DashboardHome.jsx
@@ -1,5 +1,19 @@
 import { useEffect, useState } from "react";
 import customFetch from "../../utils/customFetch";
+import { PiUsersThree } from "react-icons/pi";
+import { PiUserBold } from "react-icons/pi";
+import { PiMicrophoneStageBold } from "react-icons/pi";
+import { PiMonitorPlayFill } from "react-icons/pi";
+import { PiTrophyBold } from "react-icons/pi";
+import { MdEventNote } from "react-icons/md";
+import { RiBaseStationLine } from "react-icons/ri";
+import { ImStarFull } from "react-icons/im";
+import { MdOutlineAccessTime } from "react-icons/md";
+import { PiMagnifyingGlassPlusFill } from "react-icons/pi";
+// Location
+import { PiMapPinLineDuotone } from "react-icons/pi";
+// notices
+import { PiMegaphoneBold } from "react-icons/pi";
 
 const DashboardHome = () => {
   const [users, setUsers] = useState(0);
@@ -12,12 +26,18 @@ const DashboardHome = () => {
   const [fifth, setFifth] = useState(0);
   const [all, setAll] = useState(0);
 
+  const [skill, setSkill] = useState(0);
+  const [hack, setHack] = useState(0);
+  const [tutor, setTutor] = useState(0);
+
+  const [intern, setIntern] = useState(0);
+  const [full, setFull] = useState(0);
+  const [part, setPart] = useState(0);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await customFetch.get("/admin/members/all");
-        console.log("Response:", response);
-
         const members = response.data.members;
 
         setAll(members.length);
@@ -41,6 +61,48 @@ const DashboardHome = () => {
             setFifth((prev) => prev + 1);
           }
         });
+
+        const eventResponse = await customFetch.get("/admin/events/all");
+        const events = eventResponse.data.events;
+
+        setEvents(events.length);
+
+        setSkill(0);
+        setHack(0);
+        setTutor(0);
+
+        events.forEach((event) => {
+          if (event.eventType == "Skill-sharing") {
+            setSkill((prev) => prev + 1);
+          }
+          if (event.eventType == "Hackathon") {
+            setHack((prev) => prev + 1);
+          }
+          if (event.eventType == "Tutorial") {
+            setTutor((prev) => prev + 1);
+          }
+        });
+
+        const Oppresponse = await customFetch.get("/admin/opportunities/all");
+        const opportunities = Oppresponse.data.opportunities;
+
+        setOpportunity(opportunities.length);
+
+        setIntern(0);
+        setFull(0);
+        setPart(0);
+
+        opportunities.forEach((member) => {
+          if (member.jobType == "Internship") {
+            setIntern((prev) => prev + 1);
+          }
+          if (member.jobType == "Part-time") {
+            setPart((prev) => prev + 1);
+          }
+          if (member.jobType == "Full-time") {
+            setFull((prev) => prev + 1);
+          }
+        });
       } catch (error) {
         console.error("Error:", error);
       }
@@ -48,7 +110,156 @@ const DashboardHome = () => {
     fetchData();
   }, []);
 
-  return <></>;
+  return (
+    <section className="p-8 h-full font-poppins ">
+      <div>
+        <h4 className="mt-2 font-bold text-3xl text-slate-900">
+          Main Dashboard
+        </h4>
+      </div>
+
+      <div className="mt-14 mb-10">
+        <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm justify-center">
+          <div className="w-20 h-20 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+            <PiUsersThree className="w-9 h-10 text-primary-default" />
+          </div>
+          <div>
+            <p className="text-gray-light text-xl ">Total Members</p>
+            <p className="text-slate-900 font-bold text-2xl">{all}</p>
+          </div>
+        </div>
+
+        <div className="flex space-x-6 mt-2">
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <PiUserBold className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">3rd Year</p>
+              <p className="text-slate-900 font-bold text-2xl">{third}</p>
+            </div>
+          </div>
+
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <PiUserBold className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">4th Year</p>
+              <p className="text-slate-900 font-bold text-2xl">{fourth}</p>
+            </div>
+          </div>
+
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <PiUserBold className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">5th Year</p>
+              <p className="text-slate-900 font-bold text-2xl">{fifth}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-14 mb-10">
+        <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm justify-center">
+          <div className="w-20 h-20 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+            <MdEventNote className="w-9 h-10 text-primary-default" />
+          </div>
+          <div>
+            <p className="text-gray-light text-xl ">Total Events</p>
+            <p className="text-slate-900 font-bold text-2xl">{events}</p>
+          </div>
+        </div>
+
+        <div className="flex space-x-6 mt-2">
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <PiMicrophoneStageBold className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">
+                Skill-Sharing
+              </p>
+              <p className="text-slate-900 font-bold text-2xl">{skill}</p>
+            </div>
+          </div>
+
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <PiTrophyBold className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">
+                Hackathon
+              </p>
+              <p className="text-slate-900 font-bold text-2xl">{hack}</p>
+            </div>
+          </div>
+
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <PiMonitorPlayFill className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">Tutorial</p>
+              <p className="text-slate-900 font-bold text-2xl">{tutor}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-14 mb-10">
+        <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm justify-center">
+          <div className="w-20 h-20 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+            <PiMagnifyingGlassPlusFill className="w-9 h-10 text-primary-default" />
+          </div>
+          <div>
+            <p className="text-gray-light text-xl ">Total Opportunities</p>
+            <p className="text-slate-900 font-bold text-2xl">{opprtunity}</p>
+          </div>
+        </div>
+
+        <div className="flex space-x-6 mt-2">
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <RiBaseStationLine className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">
+                Internship
+              </p>
+              <p className="text-slate-900 font-bold text-2xl">{intern}</p>
+            </div>
+          </div>
+
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <ImStarFull className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">
+                Full-Time
+              </p>
+              <p className="text-slate-900 font-bold text-2xl">{full}</p>
+            </div>
+          </div>
+
+          <div className="bg-white rounded-lg w-full px-4 py-5 flex items-center shadow-sm">
+            <div className="w-14 h-14 rounded-full bg-primary-light mr-4 flex justify-center items-center">
+              <MdOutlineAccessTime className="w-7 h-7 text-primary-default" />
+            </div>
+            <div>
+              <p className="text-gray-light text-lg font-semibold ">
+                Part-Time
+              </p>
+              <p className="text-slate-900 font-bold text-2xl">{part}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default DashboardHome;
